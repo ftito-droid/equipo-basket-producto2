bf507daad2259e383314afecf96ea3d4a07cc7f9
rpratsro <rpratsro@uoc.edu>
2026-04-01 17:14:58 +0200

    componentes players modificados

--- src/app/components/players/players.ts
+++ src/app/components/players/players.ts
@@ -1,8 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PLAYERS } from '../../data/players';
 import { FilterPlayersPipe } from '../../pipes/filterPlayers.pipe';
+import { PlayersService } from '../../services/players';  // 👈 Importa tu servicio
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-players',
@@ -12,7 +14,9 @@ import { FilterPlayersPipe } from '../../pipes/filterPlayers.pipe';
   styleUrls: ['./players.css']
 })
 export class PlayersComponent {
-  players = PLAYERS;
+  // players = PLAYERS;
+   // Ahora usamos un Observable para los datos en tiempo real
+  players$!: Observable<any[]>;
 
   search = '';
   positionFilter = '';
@@ -21,7 +25,25 @@ export class PlayersComponent {
 
   @Output() playerSelected = new EventEmitter<any>();
 
+   // Inyectamos el servicio en el constructor
+  constructor(private playersService: PlayersService) {}
+
+  ngOnInit() {
+    // Cargamos los jugadores desde Firebase al arrancar
+    this.players$ = this.playersService.getPlayers();
+  }
+
   selectPlayer(player: any) {
     this.playerSelected.emit(player);
+  }
+
+  // Método para borrar un jugador  
+  async deletePlayer(id: string) {
+    try {
+      await this.playersService.deletePlayer(id);
+      console.log('Jugador eliminado');
+    } catch (error) {
+      console.error('Error al eliminar:', error);
+    }
   }
 }
